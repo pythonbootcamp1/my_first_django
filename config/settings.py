1a018f9b75de5cf3a218d098bde89893f3b67760
hans <lionpythonbootcamp@gmail.com>
2025-11-03 19:55:41 +0900

    modify

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'my_static',
 # ]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
